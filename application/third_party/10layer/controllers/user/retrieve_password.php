fd2a724e6197d4e4d215a6e25fc2809a15417869
j-norwood-young <jason@10layer.com>
2011-12-15 10:31:08 +0200

    Send OTP instead of password

--- application/third_party/10layer/controllers/user/retrieve_password.php
+++ application/third_party/10layer/controllers/user/retrieve_password.php
@@ -14,6 +14,8 @@
 		 */
 		public function __construct() {
 			parent::__construct();
+			$this->load->helper("password");
+			//$this->load->model("model_user");
 		}
 		
 		public function index() {
@@ -22,8 +24,10 @@
 			$email=$this->input->post("email");
 			if (!empty($email)) {
 				$password=$this->model_user->get_password_by_email($email);
-				if (!empty($email)) {
-					$this->email_password($email, $password);
+				if (!empty($password)) {
+					$otp=$this->model_user->updateOtp($email);
+					
+					$this->email_otp($email, $otp);
 					$data["email_sent"]=true;
 				}
 				
@@ -44,8 +48,17 @@
 			$this->email->to($email); 
 			
 			$this->email->subject('10Layer CMS password');
-			$this->email->message("Your 10Layer password is <strong>$password</strong");	
+			$this->email->message("Your 10Layer password is <strong>$password</strong>");	
 			
+			$this->email->send();
+		}
+		
+		protected function email_otp($email, $otp) {
+			$this->load->library("email");
+			$this->email->from('admin@10layer.com', '10Layer CMS');
+			$this->email->to($email); 
+			$this->email->subject('10Layer CMS password');
+			$this->email->message("Please go to the following link to confirm your 10Layer account details. ".base_url()."user/otplogin/$otp");	
 			$this->email->send();
 		}
 	}
