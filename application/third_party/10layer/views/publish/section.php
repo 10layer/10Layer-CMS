0d7edb243ed6196866cf4a9fff6956067166f3f4
Tumelo <tumelo@10layer.com>
2012-03-15 20:18:27 +0200

    added the auto section functionality to the puplish page

--- application/third_party/10layer/views/publish/section.php
+++ application/third_party/10layer/views/publish/section.php
@@ -2,23 +2,69 @@
 	link_js("/tlresources/file/js/publish/section_manager.js");
 ?>
 
+  
 
-<div class="message hidden" id="message"></div>
+<?php
+
+if(isset($zones[0])){
+
+?>
+
 <div id="controlls">
 
-<div id="btn_submit">
-<select name="subsections" id="subsection_selector">
-	
-<?php
-foreach($zones as $zone) {	
-?>
-	<option value="/publish/worker/subsection/<?= $section_urlid ?>/<?= $zone->urlid ?>" class="subsection_title"><?= $zone->getData()->title ?></option>
-<?php
-}
-?>
-</select>
+<input type="hidden" id="active_zone" value="/publish/worker/subsection/<?= $section_urlid ?>/<?= $zones[0]->getData()->urlid ?>">
 
+<div id="btn_submit">
+
+<div id="config_container">
+<!-- ===================================== -->
+<div id="config_section_container">
+	<a id="config_section"> Publishing to <?php echo $section_data->title; ?> </a> > <span id="active_zone_display" class="auto_<?= $zones[0]->getData()->auto ?>"><?php echo $zones[0]->getData()->title; ?></span> <div style="height:15px; width:15px; float:right;" id="config_section_options">Options</div>
+</div>
+
+
+
+<div id="section_config" class="shadow">
+
+ <div id="section_automator">
+ 	Automate -> <a option="all" class="mass_selector">All</a> | <a option="none" class="mass_selector">None</a>
+ </div>
+
+	<div id="zone_automators">
+	
+	<table class="small_table">
+		<tr>
+			<th>Automate</th>
+			<th>Zone</t>
+		</tr>
+		
+		<?php 
+		foreach($zones as $zone){
+		?>
+			
+		<tr>
+			<td align="center"><input type="checkbox" class="zone_automator" <?php echo ($zone->getData()->auto == "0") ? "" : "checked='checked'" ?> id="<?= $zone->getData()->content_id ?>"></td>
+			<th><a class="zone_selector auto_<?= $zone->getData()->auto ?>" href="/publish/worker/subsection/<?= $section_urlid ?>/<?= $zone->urlid ?>"><?= $zone->getData()->title ?></a></t>
+		</tr>
+			
+				
+		<?php
+		}
+			
+		?>
+	
+	</table>
+	
 	</div>
+
+
+ </div>
+
+</div>
+<!-- ===================================== -->
+
+
+</div>
 	
 	
 	
@@ -54,15 +100,20 @@ foreach($zones as $zone) {
 	<input type="hidden" id="section_id" name="section_id" value="<?= $section_id ?>" />
 </form>
 
-<!--
+
+
 <?php
-		foreach($zones as $zone) {	
-	?>
-		<li class="subsection_title">
-			<a id="<?= $section_urlid ?>_ajax_loader"  section="<?= $section_urlid ?>" subsection="<?= $zone->urlid ?>" class="ajax_loader" href="/publish/worker/subsection/<?= $section_urlid ?>/<?= $zone->urlid ?>"><?= $zone->getData()->title ?></a>
-		</li>
-	<?php
-		}
-	?>
-	
--->
+
+}else{
+?>
+
+<div class="big_error_message">
+	The  system has found that there are no zones defined for the selected section, Please contact the System Administrator about this issue.
+</div>
+
+<?php
+
+}
+
+?>
+
