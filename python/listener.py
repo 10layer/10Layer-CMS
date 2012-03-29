965d1b5e6f8d9f6c1a6a3b5d51d214c54cb4379e
j-norwood-young <jason@10layer.com>
2012-03-29 11:40:30 +0200

    Change 10Layer Listener logging to /var/log/10layer

--- python/listener.py
+++ python/listener.py
@@ -4,6 +4,7 @@ from tenlayer_listener import TenLayerListener
 import urllib2
 import syslog
 import ConfigParser
+import logging
 
 Config = ConfigParser.ConfigParser()
 Config.read("/etc/10layer/listener")
@@ -13,49 +14,54 @@ server=Config.get("main", "server")
 syslog.openlog("10LayerListener")
 syslog.syslog('Listener started')
 
+logging.basicConfig(filename='/var/log/10layer.log', format='%(asctime)-6s: %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('10Layer Listener logger')
+logger.setLevel(logging.INFO)
+logger.info('Listener started')
+
 def delete(message):
-	syslog.syslog(syslog.LOG_INFO,  "Caught message Delete %s %s" % (message[0], message[1]))
+	logger.info("Caught message Delete %s %s" % (message[0], message[1]))
 	urlStr="%s/workers/eventapi/delete/%s/%s" % (server, message[0], message[1])
 	try:
 		fh = urllib2.urlopen(urlStr)
 		response = fh.read()
 		fh.close()
-		syslog.syslog(syslog.LOG_INFO,  "Response: %s" % response)
+		logger.info("Response: %s" % response)
 	except IOError:
-		syslog.syslog(syslog.LOG_ERR,  'Error opening %s' % urlStr)
+		logger.error('Error opening %s' % urlStr)
 	
 def edit(message):
-	syslog.syslog(syslog.LOG_INFO,  "Caught message Edit %s %s" % (message[0], message[1]))
+	logger.info("Caught message Edit %s %s" % (message[0], message[1]))
 	urlStr="%s/workers/eventapi/edit/%s/%s" % (server, message[0], message[1])
 	try:
 		fh = urllib2.urlopen(urlStr)
 		response = fh.read()
 		fh.close()
-		syslog.syslog(syslog.LOG_INFO,  "Response: %s" % response)
+		logger.info("Response: %s" % response)
 	except IOError:
-		syslog.syslog(syslog.LOG_ERR,  'Error opening %s' % urlStr)
+		logger.error('Error opening %s' % urlStr)
 
 def create(message):
-	syslog.syslog(syslog.LOG_INFO,  "Caught message Create %s %s" % (message[0], message[1]))
+	logger.info("Caught message Create %s %s" % (message[0], message[1]))
 	urlStr="%s/workers/eventapi/create/%s/%s" % (server, message[0], message[1])
 	try:
 		fh = urllib2.urlopen(urlStr)
 		response = fh.read()
 		fh.close()
-		syslog.syslog(syslog.LOG_INFO,  "Response: %s" % response)
+		logger.info("Response: %s" % response)
 	except IOError:
-		syslog.syslog(syslog.LOG_ERR,  'Error opening %s' % urlStr)
+		logger.error('Error opening %s' % urlStr)
 
 def publish(message):
-	syslog.syslog(syslog.LOG_INFO,  "Caught message Publish %s" % (message))
+	logger.info("Caught message Publish %s" % (message))
 	urlStr="%s/workers/eventapi/publish/%s" % (server, message)
 	try:
 		fh = urllib2.urlopen(urlStr)
 		response = fh.read()
 		fh.close()
-		syslog.syslog(syslog.LOG_INFO,  "Response: %s" % response)
+		logger.info("Response: %s" % response)
 	except IOError:
-		syslog.syslog(syslog.LOG_ERR,  'Error opening %s' % urlStr)
+		logger.error('Error opening %s' % urlStr)
 	
 listener=TenLayerListener()
 listener.add_callback({"callback":delete, "func":"delete"})
