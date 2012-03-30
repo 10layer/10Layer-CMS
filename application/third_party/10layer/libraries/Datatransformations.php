58cb265a6db6df9772259b252320d6229482e556
j-norwood-young <jason@10layer.com>
2012-03-30 13:59:56 +0200

    Add a safetext data transformation and use it on titles

--- application/third_party/10layer/libraries/Datatransformations.php
+++ application/third_party/10layer/libraries/Datatransformations.php
@@ -66,5 +66,86 @@ class Datatransformations {
 	public function str_replace(&$sender, $value, $search, $replace) {
 		return str_replace($search, $replace, $value);
 	}
+	
+	public function safetext(&$sender, $s) {
+		
+		/*$s=str_replace(array('Ò','Ó'),'"', $s);
+		$s=str_replace(array(chr(145),'Õ'), "'", $s);
+		$s=str_replace(chr(226).chr(128).chr(147), "-", $s);
+		$s=str_replace("Ž", "e", $s);
+		$s=str_replace("É", "É", $s);*/
+		$s=strip_tags($s);
+		$s=$this->convert_ascii($s);
+		//$s=$this->msword_conversion($s);
+		$encoding = mb_detect_encoding($s, "UTF-8,ISO-8859-1,WINDOWS-1252");
+		
+		if ($encoding != 'UTF-8') {
+			$s=iconv($encoding, 'UTF-8//TRANSLIT', $s);
+		}
+		
+		/*print $s;
+		print "\n";
+		for($x=0; $x<strlen($s); $x++) {
+			print ord($s[$x]).",";
+		}
+		die();*/
+		return $s;
+	}
+	
+	/**
+	 * Remove any non-ASCII characters and convert known non-ASCII characters 
+	 * to their ASCII equivalents, if possible.
+	 *
+	 * @param string $string 
+	 * @return string $string
+	 * @author Jay Williams <myd3.com>
+	 * @license MIT License
+	 * @link http://gist.github.com/119517
+	 */
+	protected function convert_ascii($string) {
+		// Replace Single Curly Quotes
+		$search[]  = chr(226).chr(128).chr(152);
+		$replace[] = "'";
+		$search[]  = chr(226).chr(128).chr(153);
+		$replace[] = "'";
+		
+		// Replace Smart Double Curly Quotes
+		$search[]  = chr(226).chr(128).chr(156);
+		$replace[] = '"';
+		$search[]  = chr(226).chr(128).chr(157);
+		$replace[] = '"';
+		
+		// Replace En Dash
+		$search[]  = chr(226).chr(128).chr(147);
+		$replace[] = '--';
+		
+		// Replace Em Dash
+		$search[]  = chr(226).chr(128).chr(148);
+		$replace[] = '---';
+		
+		// Replace Bullet
+		$search[]  = chr(226).chr(128).chr(162);
+		$replace[] = '*';
+		
+		// Replace Middle Dot
+		$search[]  = chr(194).chr(183);
+		$replace[] = '*';
+		
+		// Replace Ellipsis with three consecutive dots
+		$search[]  = chr(226).chr(128).chr(166);
+		$replace[] = '...';
+		
+		$search[]  = chr(195).chr(169);
+		$replace[] = 'e';
+		
+		
+		// Apply Replacements
+		$string = str_replace($search, $replace, $string);
+		
+		// Remove any non-ASCII Characters
+		$string = preg_replace("/[^\x01-\x7F]/","", $string);
+		
+		return $string; 
+	}
 }
 ?>
