f009aadca51c501f60a44cac17aba99cbc75d789
j-norwood-young <jason@10layer.com>
2012-07-02 16:19:58 +0200

    Potential fix for photos

--- application/third_party/10layer/controllers/workers/picture.php
+++ application/third_party/10layer/controllers/workers/picture.php
@@ -72,10 +72,8 @@
 								$filename=$field->data->fields["filename"]->value;
 								$filename=str_replace('{filename}', $filename, $field->data->fields["filename"]->linkformat);
 								$tmpfile="/resources/cache/pictures/cdn/".basename($field->value);
-								print $tmpfile;
-								die();
 								if (!file_exists(".".$tmpfile)) {
-									file_put_contents(".".$tmpfile,file_get_contents($field->value));
+									file_put_contents(".".$tmpfile,file_get_contents($filename));
 								}
 								$this->_filename=$tmpfile;
 							} else {
