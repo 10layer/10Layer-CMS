0bc41e84d09c63f850e84a20599121661fd91b36
Tumelo <tumelo@10layer.com>
2012-07-18 12:26:04 +0200

    fix create snippet

--- application/third_party/10layer/views/snippets/javascript_templates.php
+++ application/third_party/10layer/views/snippets/javascript_templates.php
@@ -314,14 +314,21 @@
 
 
 <script type='text/template' id='create-field-nesteditems'>
+	
 	<label class='<%= field.label_class %>'><%= field.label %></label>
-	<div class="nesteditems_actions">
-		<input id="nestedselect_view_<%= field.tablename %>_<%= field.name %>" name="<%= field.tablename %>_<%= field.name %>" type="hidden" tablename="<%= field.tablename %>" contenttype="<%= field.contenttype %>" fieldname="<%= field.name %>" class="nestedselect <%= field.class %>" value="" <%= (field.contenttype=='mixed') ? "mixed='mixed' contenttypes='"+field.contenttypes.join(",")+"'" : '' %> />
-		<div class="nesteditems_item_label"><%= (field.data) ? field.data.fields.title.value : 'None selected' %></div>
-		<button class="nesteditems_item_button" contenttype="<%= field.contenttype %>">Change...</button>
-		<div class="section_list" ></div>
-		<br clear="both"/>
+	<div class='nested_container' contenttype="<%= field.contenttype %>"> 
+		<div class="selected_item">
+		
+			<input id="nestedselect_view_<%= field.tablename %>_<%= field.name %>" name="<%= field.tablename %>_<%= field.name %>" type="hidden" tablename="<%= field.tablename %>" contenttype="<%= field.contenttype %>" fieldname="<%= field.name %>" class="nestedselect <%= field.class %>" value="<%= field.value %>" <%= (field.contenttype=='mixed') ? "mixed='mixed' contenttypes='"+field.contenttypes.join(",")+"'" : '' %> />
+			<div class="nesteditems_item_label1"><%= (field.data) ? field.data.fields.title.value : 'None selected' %></div>
+		</div>
+		
+		<div class="nested_items"></div>
 	</div>
+<br clear="both"/>
+
+	
+	
 </script>
 
 <script type='text/template' id='edit-field-password'>
