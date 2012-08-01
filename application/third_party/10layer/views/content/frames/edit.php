af8a6c5563bc00dac50dca183d486168ab0f95ef
j-norwood-young <jason@10layer.com>
2012-08-01 11:01:04 +0200

    iMav test

--- application/third_party/10layer/views/content/frames/edit.php
+++ application/third_party/10layer/views/content/frames/edit.php
@@ -89,6 +89,7 @@
 			$('#dyncontent').html("Loading...");
 			console.log("init_list"+"<?= base_url() ?>list/jsonlist/"+content_type+"?jsoncallback=?");
 			$.getJSON("<?= base_url() ?>list/jsonlist/"+content_type+"?jsoncallback=?", {searchstring: searchstring}, function(data) {
+				console.log("Got data");
 				$('#dyncontent').html(_.template($("#listing-template").html(), {content_type: content_type, data:data}));
 				console.log("update_pagination");
 				update_pagination(content_type, data.count, 0, data.perpage );
