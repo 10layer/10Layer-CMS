509995fb976ac1fccc2fe2cc02a339d5174ae4db
Tumelo <tumelo@10layer.com>
2012-11-13 14:06:57 +0200

    section filter field for publishing

--- application/third_party/10layer/views/publish/section.php
+++ application/third_party/10layer/views/publish/section.php
@@ -79,6 +79,24 @@ if(isset($zones[0])){
 		<div id="date_slider"></div>
 	</div>
 	<div id="search">
+		<?php
+			if (sizeof($subsections) > 0) {
+		?>
+			<select style="padding-left:3px;float:left;margin-top:2px;border: 1px solid #757474;height:22px;" type="text" id="publish_section_filter">
+		
+			<option value=''> filter by subsection... </option>
+		<?php
+			foreach ($subsections as $subsection) {
+		?>
+			<option value='<?php echo $subsection->content_id; ?>'> <?php echo $subsection->title; ?> </option>
+		<?php
+			}
+		?>
+			</select>
+		<?php
+			}
+		?>
+		
 		<input style="padding-left:3px;float:left;border: 1px solid #757474;height:20px;" type="text" id="publishSearch" value="Search..." title="Hit Enter key to search" />
 		<span id="loading_icon" style="float:left; margin:5px 10px; display:none;">
 			<img src="/tlresources/file/images/loader.gif" />
